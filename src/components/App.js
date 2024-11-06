4f20d8180ff500583d1b5a080a5600ef48433f5c
flakegotgame <victorchange808@gmail.com>
2024-11-06 12:44:19 +0300

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import Nav from './Nav';  
 import hogsData from '../porkers_data'; 
-import { Card, Image, Button, Checkbox } from 'semantic-ui-react'; 
+import { Card, Image, Button, Checkbox, Modal, Header, List } from 'semantic-ui-react'; 
 
 const App = () => {
   const [hogs, setHogs] = useState(hogsData);
   const [showGreased, setShowGreased] = useState(false);
+  const [selectedHogIndex, setSelectedHogIndex] = useState(null);  
 
   const sortHogs = (criteria) => {
     const sortedHogs = [...hogs].sort((a, b) => {
@@ -20,8 +21,8 @@ const App = () => {
   };
 
   const filteredHogs = showGreased
-    ? hogs.filter(hog => hog.greased) 
-    : hogs; 
+    ? hogs.filter(hog => hog.greased)  
+    : hogs;  
 
   const deleteHog = (index) => {
     const updatedHogs = hogs.filter((_, i) => i !== index);
@@ -36,6 +37,14 @@ const App = () => {
       return hog;
     });
     setHogs(updatedHogs);
+  };
+
+  const handleHogClick = (index) => {
+    if (selectedHogIndex === index) {
+      setSelectedHogIndex(null); 
+    } else {
+      setSelectedHogIndex(index); 
+    }
   };
 
   return (
@@ -59,7 +68,7 @@ const App = () => {
       <div className="ui grid container">
         {filteredHogs.map((hog, index) => (
           <div className="ui eight wide column" key={index}>
-            <Card>
+            <Card onClick={() => handleHogClick(index)} style={{ cursor: 'pointer' }}>
               <Image src={hog.image} alt={hog.name} />
               <Card.Content>
                 <Card.Header>{hog.name}</Card.Header>
@@ -77,6 +86,37 @@ const App = () => {
                 </Button>
               </Card.Content>
             </Card>
+
+            {selectedHogIndex === index && (
+              <Modal open={selectedHogIndex === index} onClose={() => setSelectedHogIndex(null)}>
+                <Modal.Header>{hog.name} Details</Modal.Header>
+                <Modal.Content>
+                  <List>
+                    <List.Item>
+                      <List.Header>Specialty</List.Header>
+                      <List.Description>{hog.specialty}</List.Description>
+                    </List.Item>
+                    <List.Item>
+                      <List.Header>Weight</List.Header>
+                      <List.Description>{hog.weight} lbs</List.Description>
+                    </List.Item>
+                    <List.Item>
+                      <List.Header>Greased</List.Header>
+                      <List.Description>{hog.greased ? 'Yes' : 'No'}</List.Description>
+                    </List.Item>
+                    <List.Item>
+                      <List.Header>Highest Medal Achieved</List.Header>
+                      <List.Description>{hog.highestMedal}</List.Description>
+                    </List.Item>
+                  </List>
+                </Modal.Content>
+                <Modal.Actions>
+                  <Button onClick={() => setSelectedHogIndex(null)} color="black">
+                    
+                  </Button>
+                </Modal.Actions>
+              </Modal>
+            )}
           </div>
         ))}
       </div>
@@ -85,4 +125,3 @@ const App = () => {
 };
 
 export default App;
-
